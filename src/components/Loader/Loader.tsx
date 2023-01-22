f6a85063b0b6c7c5e6dd3160c488f7507e82f018
max <justmax.xex@mail.ru>
2023-01-22 06:28:19 +0100

    add loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ReactComponent as SVG } from "../../assets/svg/loader.svg";
+
+export const Loader: React.FC = () => (
+  <div className="mt-[100px]">
+    <SVG />
+  </div>
+);
