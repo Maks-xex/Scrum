775126a385b8798370d9e611014596ee98353b93
max <justmax.xex@mail.ru>
2023-01-25 15:23:31 +0100

    fixed send request json format

--- src/api/update-card-title.ts
+++ src/api/update-card-title.ts
@@ -5,6 +5,13 @@ export const updateCardTitle = async (
   data: Omit<ICard, "id" | "order">,
   id: string
 ): Promise<void> => {
+  if (data.body)
+    Object.fromEntries(
+      data.body.map((body) => [
+        body.id,
+        { img: body.img, title: body.title, order: body.order },
+      ])
+    );
   if (!DATABASE_URL) return;
   await fetch(`${DATABASE_URL}/cardList/${id}.json`, {
     method: "PUT",
